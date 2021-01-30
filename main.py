8ebc1deaf8edd1d0a20d41d64b3f81b407b349f2
VashLT <mcvicksilvaa@gmail.com>
2021-01-30 18:07:09 -0500

    Added some css to home page

--- main.py
+++ main.py
@@ -11,9 +11,16 @@ assets.debug = True
 scss = Bundle('sass\style.sass', filters='pyscss', output='gen/style.css')
 assets.register('scss_all', scss)
 
+
 @app.route('/')
 def home():
     return render_template("myte/home.html")
 
+
+@app.route('/login')
+def login():
+    return "<h1>login</h1>"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="127.0.0.1", port=8000)
