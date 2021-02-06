527cca4107bf2b6b3e2ac0ada37615b150f0121b
VashLT <mcvicksilvaa@gmail.com>
2021-02-05 22:56:26 -0500

    modifided project structure

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_assets import Environment, Bundle
+
+
+def build_myte():
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+    # SCSS rendering
+    assets = Environment(app)
+
+    assets.url = app.static_url_path
+    assets.debug = True
+
+    scss = Bundle('sass\style.sass', filters='pyscss', output='gen/style.css')
+    assets.register('scss_all', scss)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    return app
