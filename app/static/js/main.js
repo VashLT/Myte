3cd7cd1fddb014505cee747062ec6812b5182c2b
VashLT <mcvicksilvaa@gmail.com>
2021-02-05 22:53:48 -0500

    added jquery for hamburger button

---
+++ app/static/js/main.js
@@ -0,0 +1,29 @@
+$.fn.hasAttr = function (name) {
+    return this.attr(name) !== undefined;
+};
+
+$(document).ready(function () {
+    var $side_bar = $('.side-bar');
+
+    $(".collapsible").click(function () { // click on hamburger button
+        event.preventDefault();
+        $side_bar.animate({ width: "toggle" }, 300);
+        if (!$side_bar.hasAttr("state") || $side_bar.attr("state") == "hidden") {
+            $side_bar.attr("state", "active");
+        }
+        event.stopPropagation();
+    });
+
+    $side_bar.click(function () {
+        event.stopPropagation();
+    })
+
+    $("body").click(function () {
+        if ($side_bar.hasAttr("state") && $side_bar.attr("state") === "active") {
+            $side_bar.animate({ width: "toggle" }, 300);
+            $side_bar.attr("state", "hidden");
+        }
+    });
+});
+
+
