3ab3e53c95fd11dd580d2074c3788fa164f0acfb
VashLT <mcvicksilvaa@gmail.com>
2021-02-15 07:02:06 -0500

    fixed bug

--- app/auth.py
+++ app/auth.py
@@ -6,3 +6,8 @@ auth = Blueprint("auth", __name__)
 @auth.route("/login", methods=["POST", "GET"])
 def login():
     return render_template("myte/login.html")
+
+@auth.route("/register", methods=["POST", "GET"])
+def register():
+    return "<h1>Register view</h1>"
+
