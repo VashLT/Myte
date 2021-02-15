848dc172d1b6b274f30479f59b618a0c8e7c28e5
VashLT <mcvicksilvaa@gmail.com>
2021-02-15 07:03:27 -0500

    added styling to login view

--- app/views.py
+++ app/views.py
@@ -6,3 +6,8 @@ views = Blueprint("views", __name__)
 @views.route('/')
 def home():
     return render_template("myte/home.html")
+
+
+@views.route("/<name>")
+def user(name):
+    return "Welcome <strong>%s</strong>" % name
