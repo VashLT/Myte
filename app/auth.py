493acc52dccff830b6fa1721583cf922e4336972
VashLT <mcvicksilvaa@gmail.com>
2021-02-25 21:06:18 -0500

    added register and login view

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,28 @@
-from flask import Blueprint, render_template, request, flash
+from datetime import datetime
+
+from flask import Blueprint, render_template, request, flash, url_for, session
+
+from flask_wtf import FlaskForm
+
+from wtforms.fields.html5 import DateField
+from wtforms.validators import DataRequired
+from wtforms import validators, SubmitField
 
 from .models import User
 from . import db
 
+
 auth = Blueprint("auth", __name__)
+
+TIME_FORMAT = r"%Y-%m-%d"
+
+
+class DateForm(FlaskForm):
+    date = DateField(
+        'Birthdate: ',
+        format=TIME_FORMAT,
+        validators=(validators.DataRequired(),)
+    )
 
 
 @auth.route("/login", methods=["POST", "GET"])
@@ -22,4 +41,9 @@ def login():
 
 @auth.route("/register", methods=["POST", "GET"])
 def register():
-    return "<h1>Register view</h1>"
+    form = DateForm()
+    print(form.date)
+    if form.validate_on_submit():
+        session['date'] = form.date.data
+        print(request.form)
+    return render_template("myte/register.html", current_date=datetime.now().strftime(TIME_FORMAT),  form=form)
