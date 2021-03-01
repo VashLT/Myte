591de52519ee50240aabecf765eead5c83b841fc
VashLT <mcvicksilvaa@gmail.com>
2021-03-01 13:17:19 -0500

    added js to change forms

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,65 @@
+// var currentTab = 0;
+// showTab(currentTab);
+
+// function showTab(index) {
+//     var tabs = document.getElementsByClassName("tab");
+//     if (index == 0) {
+//         document.getElementById("previous-btn").style.display = "none";
+//     } else {
+//         document.getElementById("previous-btn").style.display = "flex";
+//     }
+//     if (index == (tabs.length - 1)) {
+//         document.getElementById("next-btn").innerHTML = "Enviar";
+//     } else {
+//     document.getElementById("nextBtn").innerHTML = "Siguiente";
+//     }
+//     updateStepIndicator(index)
+// }
+
+// function updateStepIndicator(index) {
+//   var i, steps = document.getElementsByClassName("step");
+//   for (i = 0; i < steps.length; i++) {
+//     steps[i].className = steps[i].className.replace(" active", "");
+//   }
+//   // active current step
+//   steps[index].className += " active";
+// }
+
+// function nextTab(index) {
+//     var tabs = document.getElementsByClassName("tab");
+//     // Exit the function if any field in the current tab is invalid:
+//     if (n == 1 && !validateForm()) return false;
+//     // Hide the current tab:
+//     tabs[currentTab].style.display = "none";
+//     currentTab = currentTab + index;
+//     if (currentTab >= tabs.length) {
+//         document.getElementById("register-form").submit();
+//         return false;
+//     }
+//     showTab(currentTab);
+// }
+
+// function validateForm() {
+//     // This function deals with validation of the form fields
+//     var x, y, i, valid = true;
+//     tabs = document.getElementsByClassName("tab");
+//     inputs = tabs[currentTab].getElementsByTagName("input");
+        
+//     for (i = 0; i < inputs.length; i++) {
+//         if (inputs[i].value == "") {
+//             // add an "invalid" class to the field:
+//             inputs[i].className += " invalid";
+//             // and set the current valid status to false:
+//             valid = false;
+//         }
+//     }
+//     if (valid) {
+//         document.getElementsByClassName("step")[currentTab].className += " finish";
+//     }
+//     return valid; // return the valid status
+// }
+
+
 $.fn.hasAttr = function (name) {
     return this.attr(name) !== undefined;
 };
