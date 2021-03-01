a21c112bc435a4c63472323cd656172f0d2a382f
VashLT <mcvicksilvaa@gmail.com>
2021-03-01 13:16:51 -0500

    updated config file

--- config.py
+++ config.py
@@ -17,13 +17,22 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 class Config:
     """ set Flask configuration vars from .env file. """
 
-    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    FLASK_DEBUG = bool(os.getenv('FLASK_DEBUG'))
     SECRET_KEY = os.getenv('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = f'{ADAPTER}:////{DB_USER}:{DB_PASSWORD}@{SERVER}//{DB_NAME}'
+    SQLALCHEMY_DATABASE_URI = f'{ADAPTER}://{DB_USER}:{DB_PASSWORD}@{SERVER}/{DB_NAME}'
+
+    MYSQL_DATABASE_HOST = SERVER
+    MYSQL_DATABASE_PORT = int(os.getenv("DB_PORT"))
+    MYSQL_DATABASE_USER = DB_USER
+    MYSQL_DATABASE_PASSWORD = DB_PASSWORD
+    MYSQL_DATABASE_DB = DB_NAME
 
     SERVER = os.getenv('SERVER')
-    PORT = os.getenv('PORT')
+    PORT = int(os.getenv('PORT'))
 
     if not bool(os.getenv('ENABLE_CACHE')):
         SEND_FILE_MAX_AGE_DEFAULT = 0
+
+    # if FLASK_DEBUG:
+    #     SQLALCHEMY_ECHO = True
