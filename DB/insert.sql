dffa6ca59059910926e2d927711b063e9de7dcde
VashLT <mcvicksilvaa@gmail.com>
2021-03-07 13:43:41 -0500

    added insert and triggers

--- DB/insert.sql
+++ DB/insert.sql
@@ -1,3 +1,34 @@
+SET @new_line = "\n";
+
+-- Rol
 INSERT INTO Rol VALUES (1, "Normal");
 INSERT INTO Rol VALUES (2, "Premium");
 INSERT INTO Rol VALUES (3, "Administrador");
+
+-- Formula
+INSERT INTO Formula (nombre, codigo_latex, fecha_creacion, creada) VALUES (
+    "Valor promedio", 
+    "\frac{1}{b-a}\cdot \int_{a}^{b}f(x)dx",
+    STR_TO_DATE("7-3-2021", "%d-%m-%Y"),
+    0
+);
+
+INSERT INTO Formula (nombre, codigo_latex, fecha_creacion, creada) VALUES (
+    "Método de Euler", 
+    CONCAT("y_{k+1} = y_k + h\cdot f(t_k, y_k)", @new_line, "t_{k+1} = t_k + h"),
+    CAST(NOW() AS DATE),
+    0
+);
+
+-- Default user
+INSERT INTO MetaUsuario VALUES (
+    "pepe1", 
+    "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79"   -- pw: hola
+);
+INSERT INTO Usuario (id_rol, nombre_usuario, nombre, fecha_registro, email, fecha_nacimiento)
+VALUES (
+    3, "pepe1", "Pepe Andres Bolivar",
+    CURDATE(), "pepe1@gmail.com", STR_TO_DATE("7/5/1995", "%d/%m/%Y")
+);
+
+-- Indice
---
+++ DB/utils.sql
@@ -0,0 +1,21 @@
+delimiter $$
+CREATE TRIGGER new_formula AFTER INSERT ON Formula
+FOR EACH ROW
+BEGIN
+    INSERT INTO Historial (id_usuario, id_formula, fecha_registro) VALUES (
+        @current_user,
+        NEW.id_formula,
+        CURDATE()
+    );
+END; 
+$$
+delimiter ;
+
+delimiter $$
+CREATE TRIGGER delete_meta AFTER DELETE ON Usuario
+FOR EACH ROW
+BEGIN
+    DELETE FROM MetaUsuario WHERE nombre_usuario = OLD.nombre_usuario;
+END; 
+$$
+delimiter ;
