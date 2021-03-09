a0b079bc74675c8227fcc81ed1108307b8d9bd68
Yuri <yurmel1701@gmail.com>
2021-03-08 23:54:53 -0500

    insert modified

--- DB/insert.sql
+++ DB/insert.sql
@@ -5,6 +5,33 @@ INSERT INTO Rol VALUES (1, "Normal");
 INSERT INTO Rol VALUES (2, "Premium");
 INSERT INTO Rol VALUES (3, "Administrador");
 
+-- Nivel educativo
+INSERT INTO NivelEducativo VALUES
+    (1, "Bachillerato"),
+    (2, "Pregrado"),
+    (3, "Posgrado")
+;
+
+-- Carrera
+INSERT INTO Carrera (nombre) VALUES
+    ("Biología"),
+    ("Física"),
+    ("Licenciatura en Matemáticas"),
+    ("Matemáticas"),
+    ("Química"),
+    ("Diseño Industrial"),
+    ("Ingeniería Civil"),
+    ("Ingeniería de Eléctrica"),
+    ("Ingeniería de Electrónica"),
+    ("Ingeniería Industrial"),
+    ("Ingeniería Mecánica"),
+    ("Ingeniería de Sistemas"),
+    ("Ingeniería Metalúrgica"),
+    ("Ingeniería de Petróleos"),
+    ("Ingeniería Química"),
+    ("Geología")
+;
+
 -- Formula
 INSERT INTO Formula (nombre, codigo_latex, fecha_creacion, creada) VALUES (
     "Valor promedio", 
