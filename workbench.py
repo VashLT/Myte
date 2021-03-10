5fe6609dc7674a855102767042a3cf2ebe95fb0d
Gacrucis <edwardpara910@gmail.com>
2021-03-10 01:56:35 -0500

    Added another model

--- workbench.py
+++ workbench.py
@@ -1,14 +1,43 @@
-from scripter import Script
+import math
+
+class Script:
+
+    def __init__(self, var_names, code):
+
+        self.var_dict = {}
+
+        for i, name in enumerate(var_names):
+            self.var_dict[name] = i
+        
+        self.code = code
+
+    def run_script(self, raw_input):
+
+        # Limit input length to ensure 
+        dict_length = len(self.var_dict)
+        
+        # Converts string of inputs into integers, also prevents bad code execution
+        raw_vars = raw_input.split(',')
+        raw_values = [float(var) for var in raw_vars]
+
+        for i, name in enumerate(self.var_dict):
+            self.var_dict[name] = raw_values[i]
+
+        local_dict = dict(self.var_dict)
+        local_dict.update({'math' : math})
+
+        # print(local_dict)
+        result = eval(self.code, local_dict)
+        
+        return result
 
 
 # test = Script(['a', 'b'], 'math.exp(a)')
 # test.run_script('1, 2')
 
-long_script = 'sum([a**n / math.factorial(n) for n in range(int(b))])'
+long_script = 'sum([x**i / math.factorial(i) for i in range(int(n))])'
 
-'math.sqrt(a^2 + b^2)'
-
-test = Script(['a', 'b'], long_script)
+test = Script(['x', 'n'], long_script)
 result = test.run_script('2, 10')
 print(result)
 
@@ -18,4 +47,4 @@ print(result)
 
 # print(sum([a**n / math.factorial(n) for n in range(b)]))
 
-print(eval(str(eval('1 + 1'))))
+# print(eval(str(eval('1 + 1'))))
