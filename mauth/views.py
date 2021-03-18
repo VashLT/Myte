1267815ef0f273078a9715e501793c288cc7486c
VashLT <mcvicksilvaa@gmail.com>
2021-03-18 16:28:04 -0500

    added some front-end to register stage and new dependencies

--- mauth/views.py
+++ mauth/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render, redirect
 
 from django.contrib import messages  # flashing
 
+from django.core.validators import validate_email
+
+from django.core.exceptions import ValidationError
+
 from .models import User, MetaUser, Rol
-from .forms import DateForm
+from .forms import RegisterForm
+from . import utils
 
 # Create your views here.
 
@@ -31,10 +36,31 @@ def login(request):
 
 
 def register(request):
-    df = DateForm()
-    if request.method == "POST":
-        print(request.POST)
-    context = {
-        "date_form": df
-    }
-    return render(request, 'mauth/register.html', context)
+    print(User.objects.all())
+    if request.method == "GET":
+        form = RegisterForm()
+    else:
+        form = RegisterForm(request.POST)
+        if validate_user(request) and form.is_valid():
+            messages.info(request, "Valid register form")
+
+    return render(request, 'mauth/register.html', {"form": form})
+
+
+def validate_user(request):
+    """
+        validate user data to create user
+    """
+    data = request.POST
+    state = True
+
+    if data["pw1"] != data["pw2"]:
+        messages.error(request, "Passwords must be the same!")
+        state = False
+    if not utils.validate_username(MetaUser, data['username']):
+        messages.error(request, "Username already exists")
+        state = False
+    if not utils.is_email(data['email']):
+        messages.error(request, "Not a valid email")
+        state = False
+    return state
