63f3cc7bebe0a0cd685396ab38e85b17e04bf6fb
Gacrucis <edwardpara910@gmail.com>
2021-10-20 10:05:41 -0500

    added formula model

--- backend/maths/models.py
+++ backend/maths/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from djongo import models
+from djongo.models.fields import ArrayField
+from django import forms
 
-# Create your models here.
+class Image(models.Model):
+
+    id_image = models.AutoField(primary_key=True)
+    added_at = models.DateTimeField()
+    url = models.CharField()
+    title = models.CharField()
+
+    class Meta:
+        abstract = True
+
+class ImageForm(forms.ModelForm):
+
+    class Meta:
+        model = Image
+        fields = (
+            'id_image', 'added_at', 'url', 'title'
+        )
+
+class Formulas(models.Model):
+
+    id_formula = models.AutoField(primary_key=True)
+    added_at = models.DateTimeField()
+    tags = models.ArrayField(
+        model_container=models.CharField, #type:ignore
+    )
+    title = models.TextField()
+    latex_code = models.CharField()
+    images = models.ArrayField(
+        model_container=Image,
+        model_form_class=ImageForm
+    )
+    is_deleted = models.BooleanField()
+    category = models.CharField()
