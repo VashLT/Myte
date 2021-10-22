86b50f74cb61b3209e89a7ebcf421a9fb2e7ce16
gamma <edwardpara910@gmail.com>
2021-10-21 22:52:01 -0500

    Fixed listfield

--- backend/maths/serializers.py
+++ backend/maths/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 from maths.models import Image, Formula
 
+class StringListField(serializers.ListField):
+
+    def to_representation(self, data):
+        """
+        List of object instances -> List of dicts of primitive datatypes.
+        """
+        raw_data = data.replace("'", "")[1:-1]
+        raw_list = raw_data.split(',')
+        return [string.strip() for string in raw_list]
+        # return [self.child.to_representation(item) if item is not None else None for item in data]
+        # return ['pene']
+
 class ImageSerializer(serializers.ModelSerializer):
     id_image = serializers.ReadOnlyField()
     class Meta:
@@ -27,8 +39,9 @@ class ImageSerializer(serializers.ModelSerializer):
 
 class FormulaSerializer(serializers.ModelSerializer):
     id_formula = serializers.ReadOnlyField()
-    tags = serializers.ListField()
-    images = serializers.ListField()
+    # tags = serializers.ListField()
+    tags = StringListField()
+    images = StringListField()
     class Meta:
         model = Formula
         fields = (
